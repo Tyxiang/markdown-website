9e65c1e89c49e10f85cff6c905d55d486c61b6b4
Tyxiang <en1434@gmail.com>
2021-09-05 20:56:55 +0800

    really one page

--- src/content.md
+++ src/content.md
@@ -1,9 +1,27 @@
-# title
+# mytitle
 
 ![](image/banner.jpg)
 
 帮助组织建立解决最常见业务挑战的关键能力并设定相应基准   
 旨在优化不断变化的全球环境中的业务性能  
+
+## header
+
+### icon
+
+`image/logo.ico`
+
+### logo
+
+![logo](image/logo.png)
+
+### nav
+
+- [unit-1](#unit-1)
+- [unit-2](#unit-2)
+- [unit-3](#unit-3)
+- [unit-4](#unit-4)
+- [unit-5](#unit-5)
 
 ## unit-1
 
@@ -143,7 +161,7 @@
 - 2
 - 3
 
-#### pop
+#### pop-1
 
 ![](image/record.svg)
 
@@ -152,3 +170,25 @@
 - 1
 - 2
 - 3
+
+#### pop-2
+
+![](image/record.svg)
+
+文字文字文字文字文字文字文字文字文字文字文字文字
+
+- 1
+- 2
+- 3
+
+## footer
+
+### nav
+
+[底部导航](#) - [底部导航](#) - [底部导航](#) - [底部导航](#) - [底部导航](#) - [底部导航](#)
+
+### ending
+
+版权所有 © 2021 北京超级管理有限公司  
+保留所有权利  
+[京 ICP 备 2020033828 号](http://www.beian.miit.gov.cn/)
--- src/index.php
+++ src/index.php
@@ -1,3 +1,120 @@
 <?php
-$request_uri = $_SERVER['REQUEST_URI'];
-echo $request_uri;
+ini_set("display_errors", "Off");
+date_default_timezone_set("Asia/Shanghai");
+include "module/Parsedown.php";
+include "module/heading-array.php";
+$content_path = "content.md";
+$content_md = file_get_contents($content_path);
+//echo $content_md;
+$Parsedown = new Parsedown();
+$content_html = $Parsedown->text($content_md);
+//echo $content_html;
+$content_array = heading_parse($content_html);
+//echo json_encode($content_array, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+//exit();
+foreach ($content_array['h1'][0]['h2'] as $h2) {
+    if ($h2['title'] == 'header') {
+        foreach ($h2['h3'] as $h3) {
+            if ($h3['title'] == 'icon') {
+                $header['icon'] = strip_tags($h3['content']);
+            }
+            if ($h3['title'] == 'logo') {
+                $header['logo'] = $h3['content'];
+            }
+            if ($h3['title'] == 'nav') {
+                $header['nav'] = $h3['content'];
+            }
+        }
+    }
+    if ($h2['title'] == 'footer') {
+        foreach ($h2['h3'] as $h3) {
+            if ($h3['title'] == 'nav') {
+                $footer['nav'] = $h3['content'];
+            }
+            if ($h3['title'] == 'ending') {
+                $footer['ending'] = $h3['content'];
+            }
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <link rel="shortcut icon" href="<?=$header['icon']?>" />
+        <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+        <link rel="stylesheet" href="css/basic.css" type="text/css">
+        <link rel="stylesheet" href="css/color.css" type="text/css">
+        <link rel="stylesheet" href="css/content.css" type="text/css">
+        <link rel="stylesheet" href="css/layout.css" type="text/css">
+        <link rel="stylesheet" href="css/layout-small.css" type="text/css">
+        <link rel="stylesheet" href="css/this.css" type="text/css">
+        <link rel="stylesheet" href="css/this-small.css" type="text/css">
+        <link rel="stylesheet" href="css/pop.min.css" type="text/css">
+        <title><?=$content_array['h1'][0]['title']?></title>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div id="logo">
+                    <a href="index.php">
+                        <?=$header['logo']?>
+                    </a>
+                </div>
+                <div class="nav">
+                    <?=$header['nav']?>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <?php
+                    echo '<div class="description">';
+                        echo $content_array['h1'][0]['content'];
+                    echo '</div>';
+                    foreach ($content_array['h1'][0]['h2'] as $i => $h2) {
+                        if ($h2['title'] != 'footer' && $h2['title'] != 'header') {
+                            echo '<div id="' . $h2['title'] . '" class="unit' . ($i % 2 === 0 ? '' : ' b') . '">';
+                                echo '<h2 class="wow animate__animated animate__bounceIn">';
+                                    echo $h2['title'];
+                                echo '</h2>';
+                                echo '<div class="wow animate__animated animate__zoomInDown">';
+                                    echo $h2['content'];
+                                echo '</div>';
+                                if (is_array($h2['h3'])) {
+                                    $n = count($h2['h3']);
+                                    foreach ($h2['h3'] as $h3) {
+                                        echo '<div class="card o' . $n . ' wow animate__animated animate__lightSpeedInRight">';
+                                        echo '<h3>' . $h3['title'] . '</h3>';
+                                        echo $h3['content'];
+                                        foreach ($h3['h4'] as $h4) {
+                                            $h4_json = json_encode($h4);
+                                            echo "<a href ='javascript:void(0);' onclick ='pop.open(" . $h4_json . ");'>";
+                                            echo $h4['title'].'&nbsp;';
+                                            echo "</a>";
+                                        }
+                                        echo '</div>';
+                                    }
+                                }
+                            echo '</div>';
+                        }
+                    }
+                ?>
+            </div>
+        </main>
+        <footer>
+            <div class="container">
+                <div class="nav"><?=$footer['nav']?></div>
+                <div class="ending"><?=$footer['ending']?></div>
+            </div>
+        </footer>
+    </body>
+</html>
+<script src="js/pop.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script>
+    new WOW().init();
+</script>
+
