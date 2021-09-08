af0e183f0187394ac0a6e053e1c8c9b7e8fcb2ce
Tyxiang <en1434@gmail.com>
2021-09-08 21:23:09 +0800

    add-necker-class

--- src/index.php
+++ src/index.php
@@ -73,7 +73,7 @@ foreach ($content_array['h1'][0]['h2'] as $h2) {
         <main>
             <div class="container">
                 <?php
-                echo '<div class="description">';
+                echo '<div class="necker description">';
                     echo $necker;
                 echo '</div>';
                 foreach ($content_array['h1'][0]['h2'] as $i => $h2) {
